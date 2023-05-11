b6b78b9b502ac3a5f6992288a3f01a40225b99ae
NungSSang <93326835+NungSSang@users.noreply.github.com>
2023-05-12 03:06:29 +0900

    feature1.cs생성 (#1)
    - feature1.cs생성
    feature branch에서 작업
    
    -feature1 씬 저장
    feature1 branch에서 작업

---
+++ Assets/feature1/feature1.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class feature1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("this is feature1 script");
+    }
+
+    // Update is called once per frame
+ 
+}
