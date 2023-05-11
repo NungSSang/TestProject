1befb81a2626c092ea77ffdf20a893dcad561077
NungSSang <kwj980421@naver.com>
2023-05-12 03:20:37 +0900

    feature3 생성
    feature3 생성
    feature3.cs 생성
    feature3 obj 생성

---
+++ Assets/feature3/feature3.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class feature3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("this is feature3");
+    }
+
+    // Update is called once per frame
+ 
+}
