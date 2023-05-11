ed5114a021d03e9fcbbc05e90652f571ef47f245
NungSSang <kwj980421@naver.com>
2023-05-12 03:13:23 +0900

    feature2생성
    feature2 폴더 및 cs 생성 및 obj 생성

---
+++ Assets/feature2/feature2.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class feature2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("this is feature2");   
+    }
+
+    // Update is called once per frame
+
+}
